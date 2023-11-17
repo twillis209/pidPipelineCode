dc51c35306264574400ceb44a1ebb50e716e0ec4
Tom Willis <tw395@cam.ac.uk>
2023-11-17 19:28:42 +0000

    Added h2 transformation function

---
+++ R/h2.R
@@ -0,0 +1,12 @@
+##' Transform heritability (h2) estimates from the observed scale to the liability scale
+##'
+##' @param h2.obs estimated h2 on the observed scale
+##' @param P prevalence of disease
+##' @param K case proportion in study
+##' @importFrom stats dnorm qnorm
+##' @export
+obs_scale_to_liab_scale <- function(h2.obs, P, K) {
+  z_2 <- dnorm(qnorm(1-K))^2
+
+  h2.obs * ((K*(1-K))^2/(P*(1-P)))/z_2
+}
