c62af0b8abf93a63c440e5bdd126d2f19b29594a
Tom Willis <tw395@cam.ac.uk>
2023-11-17 19:40:54 +0000

    Fixed swapped P and K parameter labels

--- R/h2.R
+++ R/h2.R
@@ -1,9 +1,9 @@
 ##' Transform heritability (h2) estimates from the observed scale to the liability scale
 ##'
 ##' @param h2.obs estimated h2 on the observed scale
-##' @param P prevalence of disease
-##' @param K case proportion in study
-##' @importFrom stats dnorm qnorm
+##' @param P case proportion in study
+##' @param K disease prevalence in population
+##' ##' @importFrom stats dnorm qnorm
 ##' @export
 obs_scale_to_liab_scale <- function(h2.obs, P, K) {
   z_2 <- dnorm(qnorm(1-K))^2
